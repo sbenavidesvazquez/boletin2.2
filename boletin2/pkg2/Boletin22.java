83c1f9ddbb6a68e4eff4d41f8f198a270d97ffaf
sbenavidesvazquez <sbenavidesvazquez@eniac22.danielcastelao.org>
2014-10-07 08:50:51 +0200

    boletin2.2

---
+++ boletin2/pkg2/Boletin22.java
@@ -0,0 +1,20 @@
+//area do cuadrado 
+package boletin2.pkg2;
+
+import java.util.Scanner;
+
+
+
+public class Boletin22 {
+
+
+    public static void main(String[] args) {
+    float base;
+    
+    System.out.print("base = ");
+    Scanner dato = new Scanner(System.in);
+    base=dato.nextFloat();
+    System.out.print("base= "+ base +" area="+ base*base);
+    }
+    
+}
